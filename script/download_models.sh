1344e274063f5b28574d8b3ca8b94c6079e16b35
Elaine Wang <elaine.wang@intel.com>
2020-05-07 18:39:09 +0800

    Fix model downloading scripts
    Add "sudo" to the command line of downloading models IR files
    in case Openvino is installed to /opt/intel
    
    Also correct the error message of downloading model failure
    
    Signed-off-by: Elaine Wang <elaine.wang@intel.com>

--- script/download_models.sh
+++ script/download_models.sh
@@ -14,7 +14,7 @@ model_list="face-detection-retail-0004 human-pose-estimation-0001 vehicle-licens
 
 for i in $model_list
 do
-    echo "./downloader.py --name $i  --precisions fp16"
+    echo "./downloader.py --name $i  --precisions FP16"
     ./downloader.py --name $i  --precisions FP16
 
     if [ -f "${INTEL_OPENVINO_DIR}/deployment_tools/open_model_zoo/tools/downloader/intel/$i/FP16/$i.xml" ]
@@ -22,6 +22,6 @@ do
         echo "$i was downloaded successfully"
     else
         echo "Failed to download $i model. "
-        echo "Please manually run command:${INTEL_OPENVINO_DIR}/deployment_tools/tools/model_downloader/downloader.py --name $i --precisions fp16" 
+        echo "Please manually run command:${INTEL_OPENVINO_DIR}/deployment_tools/tools/model_downloader/downloader.py --name $i --precisions FP16" 
     fi
 done
